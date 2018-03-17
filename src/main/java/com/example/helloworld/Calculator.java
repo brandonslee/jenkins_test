85535a4af2705912b70ae1656c46c87a5049365c
gplayer <gaplessplayer2@gmail.com>
2018-03-17 15:43:17 +0900

    add more

--- src/main/java/com/example/helloworld/Calculator.java
+++ src/main/java/com/example/helloworld/Calculator.java
@@ -1,4 +1,14 @@
 package com.example.helloworld;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Calculator {
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
 }
--- src/test/java/com/example/helloworld/CalculatorTest.java
+++ src/test/java/com/example/helloworld/CalculatorTest.java
@@ -1,4 +1,19 @@
 package com.example.helloworld;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(3, calculator.subtract( 7, 4));
+    }
 }
